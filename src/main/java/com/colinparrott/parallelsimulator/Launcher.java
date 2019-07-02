f45ba57d830f454a8c2c4c9a4f01adc3415d8614
Colin Parrott <colinparrott2@gmail.com>
2019-07-02 19:26:23 +0100

    Now uses Shadow Jar to create fat jar with all dependencies

--- src/main/java/com/colinparrott/parallelsimulator/Launcher.java
+++ src/main/java/com/colinparrott/parallelsimulator/Launcher.java
@@ -1,12 +1,13 @@
 package com.colinparrott.parallelsimulator;
 
-import com.colinparrott.parallelsimulator.engine.simulator.CLISimulator;
+import com.colinparrott.parallelsimulator.engine.simulator.gui.GUISimulator;
 
 public class Launcher
 {
     public static void main(String[] args)
     {
-        CLISimulator simulator = new CLISimulator(args);
-        simulator.originalCLI();
+//        CLISimulator simulator = new CLISimulator(args);
+//        simulator.originalCLI();
+        GUISimulator.main(args);
     }
 }
